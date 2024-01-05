2be95f0e4a33c47407afb25436092f68250cded3
maboo <maboo@Mabood.myfiosgateway.com>
2024-01-05 17:26:55 -0500

    Initial Commit

---
+++ src/c/DemoC.java
@@ -0,0 +1,11 @@
+package c;
+
+public class DemoC {
+	public static void main(String[] args) {
+		
+		System.out.println("Java is the Best");
+		
+		System.out.println("Working on project");
+	}
+
+}
